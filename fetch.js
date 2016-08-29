bcc3f801bf6ba9889d7b64e16f8b50f50b3d0a7c
Andri Möll <andri@dot.ee>
2016-08-29 07:54:24 +0000

    Reorder imports.

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
-var request = require("./request")
 var Response = require("./response")
+var request = require("./request")
 
 module.exports = function fetch(url, opts) {
 	return request(url, opts).then(function(res) { return new Response(res) })
